53c56fc39df2197af98453879998fcbecb84ac4c
Akshay-IndieDev <smartscorpius.sting918@gmail.com>
2020-10-31 04:04:22 +0530

    Added animations

---
+++ WrongJump/Assets/Scripts/movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.setFloat("Horizontal", Input.GetAxis("Horizontal"));
+        Vector3 horizontal = new Vector3 (Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;        
+    }
+}
