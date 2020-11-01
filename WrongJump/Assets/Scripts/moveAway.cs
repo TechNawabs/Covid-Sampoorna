86293967b3f5c9708433ff5b6b64b8f4a19378f0
Akshay-IndieDev <smartscorpius.sting918@gmail.com>
2020-11-01 14:17:16 +0530

    update 2

---
+++ WrongJump/Assets/Scripts/moveAway.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveAway : MonoBehaviour
+{
+    public Vector3 destination;
+    private Animation animation;
+
+    void Start()
+    {   
+        animation = GetComponent<Animation>();
+        StartCoroutine("backToStart");
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime);
+    }
+
+    IEnumerator backToStart(){
+        yield return new WaitForSeconds(2000);
+        if(transform.position == destination){
+            animation.Play();
+        }
+    }
+    
+}
